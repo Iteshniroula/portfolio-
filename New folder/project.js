86d7b9df141b514ed0b2c7fd73f9a6d33e6650b2
Iteshniroula <iteshniroulaislingtonedu@gmail.com>
2022-12-20 21:11:46 +0545

    portfilo

---
+++ New folder/project.js
@@ -0,0 +1,12 @@
+
+function msg()   // display alert message
+                {
+
+                    alert("Please fill the form");
+                }
+var d = new Date(); // displaying date and time
+document.getElementById("demo").innerHTML = d;
+
+
+
+
